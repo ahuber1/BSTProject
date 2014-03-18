6df576559840d0a4f085b841d37299d198167282
mjudy <mjudy1@umbc.edu>
2014-03-18 18:48:06 -0400

    RegEx pattern WORKS

--- src/proj2/BinaryNode.java
+++ src/proj2/BinaryNode.java
@@ -6,7 +6,7 @@ package proj2;
  *          Date: 3/16/14
  *          Time: 1:30 AM
  */
-public class BinaryNode<E>
+public class BinaryNode<E extends Comparable<? super E>>
 {
     private E element;
     private BinaryNode<E> left;
@@ -33,5 +33,25 @@ public class BinaryNode<E>
     public void setElement(E element)
     {
         this.element = element;
+    }
+
+    public BinaryNode<E> getLeft()
+    {
+        return left;
+    }
+
+    public BinaryNode<E> getRight()
+    {
+        return right;
+    }
+
+    public void setLeft(E element)
+    {
+        left.element = element;
+    }
+
+    public void setRight(E element)
+    {
+        right.element = element;
     }
 }
